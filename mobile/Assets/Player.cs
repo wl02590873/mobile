5dd0890a6bcaa6fe1c11741d4f956df2c909751e
wl02590873 <51389151+wl02590873@users.noreply.github.com>
2019-07-29 16:28:02 +0800

    0729

--- mobile/Assets/Player.cs
+++ mobile/Assets/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     private void Awake()
     {
+        ClampXAxisRotationToValue(0);
         LockCursor();
         xAxisClamp = 0.0f;
     }
@@ -16,78 +17,32 @@ public class Player : MonoBehaviour
     }
 
     public float speed = 10;
-    [Header("跳躍高度")]
-    public float JumpHeight = 10;
+
     [Header("滑鼠靈敏度")]
     public float mouseSpeed;
     //X軸最大90度
     private float xAxisClamp;
-
-    float Gravity = 200;//重力
-
-    bool OnGround = false;//地面
-
-    float DistanceToGround;//地面距離
-
-    Vector3 GroundNomal;
-
-    private Rigidbody rb;
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;//鎖定Rigidbody旋轉
     }
 
     void Update()
     {
         playerCamera();
-        playerMobile();
-        #region 地面控制
-        RaycastHit hit = new RaycastHit();
-        if (Physics.Raycast(transform.position, -transform.up, out hit, 20))
-        {
-            DistanceToGround = hit.distance;
-            GroundNomal = hit.normal;
-            if (DistanceToGround <= 2.0f)
-            {
-                Debug.DrawLine(transform.position, hit.point, Color.red);
-                OnGround = true;
-            }
-            else
-            {
-                OnGround = false;
-            }
-        }
-        #endregion
     }
-    private void playerMobile()
-    {
-        #region 移動
-        float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        float z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
-        transform.Translate(x, 0, z);
-        #endregion
-
-        #region 跳躍
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.AddForce(transform.up * 4000 * JumpHeight * Time.deltaTime);
-        }
-        #endregion
-
-    }
-
     private void playerCamera()
     {
         #region 視角
         float mouseX = Input.GetAxis("Mouse X") * mouseSpeed * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSpeed * Time.deltaTime;
-        mouseX += xAxisClamp;
-        if (xAxisClamp >= 90)
+
+        mouseY += xAxisClamp;
+
+        if (xAxisClamp > 90)
         {
             xAxisClamp = 90.0f;
             mouseY = 0.0f;
-            ClampXAxisRotationToValue(270.0f);
+            ClampXAxisRotationToValue(90);
         }
         else if (xAxisClamp < -90)
         {
