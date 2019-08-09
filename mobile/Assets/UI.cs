e9d387369075f3673b3131a8cf2f0408f96c2d4d
wl02590873 <51389151+wl02590873@users.noreply.github.com>
2019-08-09 11:46:02 +0800

    UI開啟關閉

---
+++ mobile/Assets/UI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    [Header("標題UI")]
+    public GameObject titleUI;
+    [Header("新增房間UI")]
+    public GameObject NewRoom;
+    [Header("加入房間UI")]
+    public GameObject JoinUI;
+
+
+    public void title(int UIint)
+    {
+        if (UIint == 0)
+        {
+        titleUI.SetActive(false);
+        }
+        if (UIint == 1)
+        {
+            NewRoom.SetActive(true);
+            JoinUI.SetActive(false);
+        }
+        if (UIint == 2)
+        {
+            JoinUI.SetActive(true);
+            NewRoom.SetActive(false);
+        }
+    }
+}
